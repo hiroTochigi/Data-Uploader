d5610e8f5c0b92f83a250e443c0a22873eb97b99
Hiroyuki Terauchi <hterauchi@aegion.com>
2020-08-05 10:19:20 -0500

    delete memo

--- src/App.js
+++ src/App.js
@@ -88,32 +88,3 @@ export default App;
 /*
 `query { boards(ids:[${boardIds}]) { items { name, id, group{ id }, column_values { id, value } } }}` take all board items with data
 */
-
-/*
-0: {title: "Name", type: "name", settings_str: "{}", pos: null, id: "name"}
-1: {title: "Subitems", type: "subtasks", settings_str: "{"allowMultipleItems":true,"itemTypeName":"column.…splayType":"BOARD_INLINE","boardIds":[645356584]}", pos: null, id: "subitems7"}
-2: {title: "Customer", type: "text", settings_str: "{}", pos: null, id: "customer"}
-3: {title: "REFERENCE#", type: "text", settings_str: "{}", pos: null, id: "reference_"}
-4: {title: "Projected Completion", type: "date", settings_str: "{"show_current_year":true}", pos: null, id: "projected_completion"}
-5: {title: "CAT#", type: "formula", settings_str: "{"formula":"LEFT({acs_part_}, 3)"}", pos: null, id: "formula"}
-6: {title: "3rd Item Number", type: "text", settings_str: "{}", pos: null, id: "acs_part_"}
-7: {title: "BOM Updated Date", type: "date", settings_str: "{"hide_footer":false}", pos: null, id: "date6"}
-8: {title: "Full Item Description", type: "long-text", settings_str: "{}", pos: null, id: "long_text2"}
-9: {title: "WO DESCRIPTION", type: "text", settings_str: "{}", pos: null, id: "wo_description"}
-10: {title: "QTY", type: "numeric", settings_str: "{}", pos: null, id: "qty"}
-11: {title: "PARENT WO#", type: "text", settings_str: "{}", pos: null, id: "parent_wo_1"}
-12: {title: "Order Date", type: "date", settings_str: "{"show_current_year":true}", pos: null, id: "order_date"}
-13: {title: "Release Date", type: "date", settings_str: "{"show_current_year":true}", pos: null, id: "date"}
-14: {title: "Received Date", type: "date", settings_str: "{"show_current_year":true}", pos: null, id: "date3"}
-15: {title: "Revised Part List Date", type: "date", settings_str: "{"show_current_year":true}", pos: null, id: "order_date9"}
-16: {title: "BOM Changed?", type: "color", settings_str: "{"labels":{"1":"No","2":"Changed"},"labels_positions_v2":{"1":0,"2":2,"5":1}}", pos: null, id: "status_15"}
-17: {title: "ALL PARTS ORDERED", type: "color", settings_str: "{"labels":{"1":"True","2":"False","5":"","17":"Inv…"labels_positions_v2":{"1":0,"2":2,"5":1,"17":3}}", pos: null, id: "status6"}
-18: {title: "ALL PARTS RCVD", type: "color", settings_str: "{"done_colors":[1],"color_mapping":{"3":3,"6":6},"…ions_v2":{"0":0,"1":1,"2":2,"3":5,"5":3,"103":4}}", pos: null, id: "status2"}
-19: {title: "ASSEMBLY", type: "color", settings_str: "{"done_colors":[1],"color_mapping":{"0":16,"2":12,…v2":{"0":0,"1":1,"2":2,"3":5,"5":3,"7":4,"10":6}}", pos: null, id: "status4"}
-20: {title: "QC/TESTING", type: "color", settings_str: "{"done_colors":[1],"color_mapping":{"0":16,"2":12,…v2":{"0":0,"1":1,"2":2,"5":3,"7":4,"9":5,"10":6}}", pos: null, id: "status0"}
-21: {title: "Status", type: "color", settings_str: "{"done_colors":[19],"color_mapping":{"0":9,"1":19,…2":{"0":0,"1":1,"2":4,"3":5,"5":3,"12":6,"19":2}}", pos: null, id: "status"}
-22: {title: "On Hold Causes", type: "dropdown", settings_str: "{"hide_footer":false,"labels":[{"id":1,"name":"Tag…me":"BOM change"},{"id":5,"name":"Parts Delay"}]}", pos: null, id: "unsafe_closed_causes"}
-23: {title: "Close Safely?", type: "color", settings_str: "{"labels":{"1":"Safe","5":"","9":"Caution","11":"W…"labels_positions_v2":{"1":0,"5":1,"9":3,"11":2}}", pos: null, id: "status_1"}
-24: {title: "Unsafe Closed Causes", type: "dropdown", settings_str: "{"hide_footer":false,"labels":[{"id":1,"name":"No …locks on test"},{"id":5,"name":"Partial Issue"}]}", pos: null, id: "dropdown"}
-25: {title: "Closed Date", type: "date", settings_str: "{}", pos: null, id: "date4"}
-*/
